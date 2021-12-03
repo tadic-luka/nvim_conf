d3a7cf310fb08982aabe8398e706ef322e4dc706
Luka Tadic <luka.tadic365@gmail.com>
2021-12-03 23:01:42 +0100

    updated colorscheme config

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -7,6 +7,7 @@ require 'config.rust'
 require 'config.bufferline'
 
 vim.o.background = "dark"
+vim.g.gruvbox_material_cursor = 'green'
+-- had to disable this so that comments don't have weird background
+vim.g.gruvbox_material_disable_italic_comment = 1
 vim.cmd('colorscheme gruvbox-material')
-vim.g['gruvbox_material_background'] = medium
-vim.g['gruvbox_material_cursos'] = green
