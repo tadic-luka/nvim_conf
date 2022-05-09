3baca8199754a8b4c01c702dec7576657935ab10
Luka Tadic <luka.tadic365@gmail.com>
2022-05-10 01:24:26 +0200

    add mapping for moving selection in telescope in insert mode

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,8 +1,15 @@
 local telescope = require 'telescope'
+local actions = require 'telescope.actions'
 
 telescope.setup {
   defaults = {
     prompt_prefix = "   ",
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      }
+    },
   },
   extensions = {
    ["ui-select"] = {
