288dfd8dab9271a75a2299b9125d66e764b3b0c4
Luka Tadic <luka.tadic365@gmail.com>
2022-08-03 18:39:48 +0200

    enable indent in treesitter

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -14,6 +14,10 @@ ts.setup {
       node_decremental = '<S-TAB>',
     },
   },
+  indent = {
+    enable = true,
+  },
+
   -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
   -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
   -- Using this option may slow down your editor, and you may see some duplicate highlights.
